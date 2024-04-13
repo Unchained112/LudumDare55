1527fe9d274cf4db37b916ebcbdfeea070f75892
Unchained112 <chengbosen112@gmail.com>
2024-04-14 00:59:37 +0200

    Add item list draft

---
+++ scripts/item.gd
@@ -0,0 +1,11 @@
+extends Control
+
+@export var id: int = 0
+
+@onready var item_bg: ColorRect = $ItemColor
+
+func _on_gui_input(event: InputEvent):
+	if event.is_pressed():
+		item_bg.color = Color8(180, 180, 180, 60)
+	else:
+		item_bg.color = Color8(125, 125, 125, 60)

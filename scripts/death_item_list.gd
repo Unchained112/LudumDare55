598704171d4647e5809e2caeadfbd80148e77a92
Unchained112 <chengbosen112@gmail.com>
2024-04-14 13:23:00 +0200

    Update Taiji color, update item bg color

--- scripts/death_item_list.gd
+++ scripts/death_item_list.gd
@@ -1,6 +1,9 @@
 extends HBoxContainer
 signal choose(choose_name)
 
+@export var default_color: Color = Color8(120, 120, 120, 60)
+@export var highlight_color: Color = Color8(180, 180, 180, 120)
+
 # 保存上次点击的节点
 var last_clicked_node = null
 
@@ -17,13 +20,13 @@ func _on_choose(event: InputEvent,control):
 		choose.emit(control.id)
 		# 如果上次点击的节点存在，恢复它的原始颜色
 		if last_clicked_node and last_clicked_node != control:
-			last_clicked_node.get_node("ItemColor").color = Color8(125, 125, 125, 60)  # 恢复原始颜色为白色
+			last_clicked_node.get_node("ItemColor").color = default_color 
 
 		# 设置当前点击的节点为高亮色
-		control.get_node("ItemColor").color = Color(1, 0.843, 0, 1)
+		control.get_node("ItemColor").color = highlight_color
 		last_clicked_node = control  # 更新上次点击的节点为当前节点
 
-func _on_nature_item_list_choose(choose_name):
+func _on_nature_item_list_choose(_choose_name):
 	if last_clicked_node:
-		last_clicked_node.get_node("ItemColor").color = Color8(125, 125, 125, 60)
+		last_clicked_node.get_node("ItemColor").color = default_color
 		last_clicked_node = null
--- scripts/nature_item_list.gd
+++ scripts/nature_item_list.gd
@@ -1,11 +1,13 @@
 extends HBoxContainer
 signal choose(choose_name)
 
+@export var default_color: Color = Color8(120, 120, 120, 60)
+@export var highlight_color: Color = Color8(180, 180, 180, 120)
+
 # 保存上次点击的节点
 var last_clicked_node = null
 
 func _ready():
-	
 	# 连接所有 Label 节点的 "mouse_entered" 信号到 _on_label_mouse_entered 函数
 	for child in get_children():
 		if child is Control:
@@ -19,14 +21,13 @@ func _on_choose(event: InputEvent,control):
 		choose.emit(control.id)
 		# 如果上次点击的节点存在，恢复它的原始颜色
 		if last_clicked_node and last_clicked_node != control:
-			last_clicked_node.get_node("ItemColor").color = Color8(125, 125, 125, 60)  # 恢复原始颜色为白色
+			last_clicked_node.get_node("ItemColor").color = default_color
 		
 		# 设置当前点击的节点为金色
-		control.get_node("ItemColor").color = Color(1, 0.843, 0, 1)  #.ItemColor.color = Color(1, 0.843, 0, 1)  # 设置为金色
+		control.get_node("ItemColor").color = highlight_color
 		last_clicked_node = control  # 更新上次点击的节点为当前节点
 
-
-func _on_death_item_list_choose(choose_name):
+func _on_death_item_list_choose(_choose_name):
 	if last_clicked_node:
-		last_clicked_node.get_node("ItemColor").color = Color8(125, 125, 125, 60)
+		last_clicked_node.get_node("ItemColor").color = default_color
 		last_clicked_node = null
